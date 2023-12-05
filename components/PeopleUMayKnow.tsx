a8bd2b4069e0e9bb6cf73f7c5a8ef60319332b9e
Muhammad Muzammil <130209360+Muzammil23456@users.noreply.github.com>
2023-12-05 12:42:18 +0500

    Update PeopleUMayKnow.tsx

--- components/PeopleUMayKnow.tsx
+++ components/PeopleUMayKnow.tsx
@@ -9,11 +9,7 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   updateDoc,
   doc,
-  deleteDoc,
   arrayRemove,
-  FieldValue,
-  deleteField,
-  where,
 } from "@firebase/firestore";
 import {
   AlertDialog,
@@ -28,17 +24,13 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useEffect, useState } from "react";
 import { RootState } from "@/app/GlobalRedux/store";
 import { setPeopleUMayKnow } from "@/app/GlobalRedux/Features/PeopleUMayKnow/PeopleUMayKnowSlice";
-import { collection, query,arrayUnion, onSnapshot } from "@firebase/firestore";
+import { collection, query, arrayUnion, onSnapshot } from "@firebase/firestore";
 import { db } from "@/modules/filebase";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { auth } from "@/modules/fileauth";
-import { FirebaseError } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
-type user = {
-  email: string;
-};
 type cardData = {
   name: string;
   role: string;
@@ -46,12 +38,19 @@ type cardData = {
   id: string;
   Request?: [{ Name: string; Uid: string; Status: string; Request: string; Role: string }];
 };
+type data = {
+  name: string;
+  role: string;
+  uid: string;
+  id: string;
+  Request?: [{ Name: string; Uid: string; Status: string; Request: string; Role: string }];
+}
 
 
 const PeopleUMayKnow = () => {
-  const [data3, setData3] = useState([]);
-  const [data2, setData2] = useState([]);
-  const [data4, setData4] = useState([]);
+  const [data3, setData3] = useState<data[]>([]);
+  const [data2, setData2] = useState<data[]>([]);
+  const [data4, setData4] = useState<data[]>([]);
   const [clicked, setClicked] = useState("");
   const [reRender, setReRender] = useState("");
   const [request, setRequest] = useState(false);
@@ -83,8 +82,10 @@ const PeopleUMayKnow = () => {
         });
       });
       setData2(testarr2);
-      setData3(testarr2.filter((a: any) => a.uid != auth.currentUser?.uid));
+      setData3(testarr2.filter((a: any) => a.uid != auth.currentUser?.uid || !a.Request?.map((e: any) => {e.Status == 'Accepted'})));
       setData4(testarr2.filter((a: any) => a.uid == auth.currentUser?.uid))
+      console.log(testarr2.filter((a: any) => a.uid != auth.currentUser?.uid && a.Request?.map((e: any) => {e.Status != 'Accepted'})));
+
     });
     return unsub2;
   };
@@ -101,28 +102,28 @@ const PeopleUMayKnow = () => {
     const name = auth.currentUser?.displayName;
     const docRef = doc(db, "user", `${id}`);
     await updateDoc(docRef, {
-      Request: 
-      arrayUnion(
-        {
-          Name: name,
-          Uid: uid2,
-          Role: role,
-          Status: "Pending",
-          Request: "Received",
-          Sender: data4[0].id
-        }),
-      
+      Request:
+        arrayUnion(
+          {
+            Name: name,
+            Uid: uid2,
+            Role: role,
+            Status: "Pending",
+            Request: "Received",
+            Sender: data4[0].id
+          }),
+
     }).then(() => {
       setRequest(true);
     });
     // console.log(name,uid,id)
   };
 
-  const friendsAdd = async (name: string,uid: string) => {
+  const friendsAdd = async (name: string, uid: string) => {
     const user = data2.filter((a: any) => a.uid == auth.currentUser?.uid);
     const docRef = doc(db, "user", `${user[0]?.id}`);
     await updateDoc(docRef, {
-      Friends: arrayUnion({ Name: name, Request: "Sent", Status: "Pending",Uid: uid }),
+      Friends: arrayUnion({ Name: name, Request: "Sent", Status: "Pending", Uid: uid }),
     });
   };
 
@@ -142,11 +143,11 @@ const PeopleUMayKnow = () => {
     }).then(() => setClicked(""));
   };
 
-  const friendsRemove = async (name: string,uid: string) => {
+  const friendsRemove = async (name: string, uid: string) => {
     const user = data2.filter((a: any) => a.uid == auth.currentUser?.uid);
     const docRef = doc(db, "user", `${user[0]?.id}`);
     await updateDoc(docRef, {
-      Friends: arrayRemove({ Name: name, Request: "Sent", Status: "Pending",Uid: uid }),
+      Friends: arrayRemove({ Name: name, Request: "Sent", Status: "Pending", Uid: uid }),
     });
   };
 
@@ -161,11 +162,11 @@ const PeopleUMayKnow = () => {
             <AlertDialogDescription asChild>
               <ScrollArea
                 type="auto"
-                className=" sm:h-[13.5rem] h-[11.5rem] p-2 rounded-md border"
+                className=" sm:h-[13.5rem] h-[11.5rem] p-1 rounded-md border"
               >
                 {data3.map((arr: cardData, i) => (
                   <Card key={i} className="m-2 flex justify-between ">
-                    <CardHeader className="  p-4 sm:p-6  ">
+                    <CardHeader className="p-[0.65rem] sm:p-6">
                       <div className="flex gap-2">
                         <Avatar>
                           <AvatarFallback className="uppercase text-sm">
@@ -173,30 +174,30 @@ const PeopleUMayKnow = () => {
                           </AvatarFallback>
                         </Avatar>
                         <div className="flex flex-col items-start">
-                          <CardTitle>{arr.name}</CardTitle>
+                          <CardTitle className="text-justify text-[.95rem] sm:text-base">{arr.name}</CardTitle>
                           <CardDescription>{arr.role}</CardDescription>
                         </div>
                       </div>
                     </CardHeader>
                     <div className=" flex px-2 items-center">
-                      {`${data4.map((e:any) => e.Friends?.map((friend: any) => friend.Uid)).flat().find((a)=>(a == arr.uid ))}` !== `${arr.uid}` && (
+                      {`${data4.map((e: any) => e.Friends?.map((friend: any) => friend.Uid)).flat().find((a) => (a == arr.uid))}` !== `${arr.uid}` && (
                         <button
                           onClick={() => (
                             getuser(),
                             requestSend(arr.id, arr.uid),
-                            friendsAdd(arr.name,arr.uid),
-                            console.log(`${data4.map((e:any) => e.Friends?.map((friend: any) => friend.Uid)).flat().find((a)=>(a == arr.uid ))}`),
+                            friendsAdd(arr.name, arr.uid),
+                            console.log(`${data4.map((e: any) => e.Friends?.map((friend: any) => friend.Uid)).flat().find((a) => (a == arr.uid))}`)
                           )}
                           className="border-solid border-[3px] disabled:!bg-pink-920 disabled:opacity-60 disabled:border-pink-920 border-pink-910 !bg-pink-910 hover:!bg-pink-920 hover:border-pink-920 hover:transition-all hover:duration-300  sm:p-2 p-1 rounded sm:w-28 sm:text-sm text-xs text-center font-semibold text-white "
                         >
                           <p>Add Friend</p>
                         </button>
                       )}
-                      {`${data4.map((e: any) => e.Friends?.map((friend: any) => friend.Uid)).flat().find((a)=>(a == arr.uid ))}` === `${arr.uid}` && (
+                      {`${data4.map((e: any) => e.Friends?.map((friend: any) => friend.Uid)).flat().find((a) => (a == arr.uid))}` === `${arr.uid}` && (
                         <button
                           onClick={() => (
                             requestCancel(arr.id, arr.role),
-                            friendsRemove(arr.name,arr.uid)
+                            friendsRemove(arr.name, arr.uid)
                           )}
                           className="border-solid border-[3px] disabled:!bg-pink-920 disabled:opacity-60 disabled:border-pink-920 border-pink-910 !bg-pink-910 hover:!bg-pink-920 hover:border-pink-920 hover:transition-all hover:duration-300  sm:p-2 p-1 rounded sm:w-28 sm:text-sm text-xs text-center font-semibold text-white "
                         >
