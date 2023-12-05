cebfd15fa91183e4d49e94e5cd15e6dab8938038
Muhammad Muzammil <130209360+Muzammil23456@users.noreply.github.com>
2023-12-05 12:48:12 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const [firebase, setFirebase] = useState(false);
   const [open,setOpen] = useState(false)
 
-  document.addEventListener("DOMContentLoaded", function(event){
-    alert(event)
-  });
-
   useEffect(() => {
     if (auth11) {
       setTimeout(() => setFirebase(true), 500);
