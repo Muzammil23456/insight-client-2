d084e9b2a559af50d05f07729b71e22e9d84ca62
Muhammad Muzammil <130209360+Muzammil23456@users.noreply.github.com>
2023-12-09 23:13:43 +0500

    Update DividedFav.tsx

--- components/Table5/DividedFav.tsx
+++ components/Table5/DividedFav.tsx
@@ -147,8 +147,7 @@ const DividedFav = ({ ondata, ondata2 }: onDataType) => {
         </button>
       )}
       <Table>
-        {!loading &&
-            data3.every((e) => e.Friends?.length < 1 || !e.Friends) && (<TableCaption>No Record Found!</TableCaption>)}
+        {!loading && Fav.length === 0 && (<TableCaption>No Record Found!</TableCaption>)}
         <TableHeader>
           <TableRow>
           <TableHead className="md:w-[20%]">UID</TableHead>
