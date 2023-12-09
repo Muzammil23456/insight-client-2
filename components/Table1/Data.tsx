ed0db9c484ec95877a6faba2555411c1a79f8613
Muhammad Muzammil <130209360+Muzammil23456@users.noreply.github.com>
2023-12-09 09:54:30 +0500

    Update Data.tsx

--- components/Table1/Data.tsx
+++ components/Table1/Data.tsx
@@ -7,6 +7,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  TableCaption
 } from "@/components/ui/table";
 import {
   Tooltip,
@@ -199,6 +200,8 @@ const Data = ({ ondata, ondata2 }: onDataType) => {
         />
       </div>
       <Table>
+        {!loading &&
+            data3.every((e) => e.Friends?.length < 1 || !e.Friends) && (<TableCaption>No Record Found!</TableCaption>)}
         <TableHeader>
           <TableRow>
             <TableHead className="md:w-[25%]">ID</TableHead>
@@ -220,16 +223,6 @@ const Data = ({ ondata, ondata2 }: onDataType) => {
                   src={loader.src}
                   alt="spinner-frame-1"
                 />
-              </TableCell>
-              <TableCell />
-            </TableRow>
-          )}
-          {!loading && data2.length === 0 && (
-            <TableRow>
-              <TableCell />
-              <TableCell />
-              <TableCell className="flex justify-start">
-                <p className="font-medium">No Record Found!</p>
               </TableCell>
               <TableCell />
             </TableRow>
