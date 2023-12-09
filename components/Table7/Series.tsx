1355403bd4f502bd88a3ee19ddc86651b3cd3b35
Muhammad Muzammil <130209360+Muzammil23456@users.noreply.github.com>
2023-12-09 10:05:24 +0500

    Update Series.tsx

--- components/Table7/Series.tsx
+++ components/Table7/Series.tsx
@@ -7,6 +7,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  TableCaption
 } from "@/components/ui/table";
 import {
   Tooltip,
@@ -139,6 +140,8 @@ const Series = ({ ondata, ondata2 }: onDataType) => {
         </button>
       )}
       <Table>
+        {!loading &&
+            data3.every((e) => e.Friends?.length < 1 || !e.Friends) && (<TableCaption>No Record Found!</TableCaption>)}
         <TableHeader>
           <TableRow>
             <TableHead className="md:w-[25%]">ID</TableHead>
@@ -160,16 +163,6 @@ const Series = ({ ondata, ondata2 }: onDataType) => {
                   src={loader.src}
                   alt="spinner-frame-1"
                 />
-              </TableCell>
-              <TableCell />
-            </TableRow>
-          )}
-          {!loading && Fav.length === 0 && (
-            <TableRow>
-              <TableCell />
-              <TableCell />
-              <TableCell className="flex justify-start">
-                <p className="font-medium">No Record Found!</p>
               </TableCell>
               <TableCell />
             </TableRow>
